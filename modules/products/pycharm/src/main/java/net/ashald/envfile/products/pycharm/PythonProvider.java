71eb27d025f9cf884d4bc4ce4a79c822f46934fa
Hassan Abouelela <hassan@hassanamr.com>
2022-09-19 13:32:09 +0400

    Fix Python Console Crash
    Fix a bug that caused the python interactive console to crash on startup
    when attempting to load environment variables.
    
    Signed-off-by: Hassan Abouelela <hassan@hassanamr.com>

--- modules/products/pycharm/src/main/java/net/ashald/envfile/products/pycharm/PythonProvider.java
+++ modules/products/pycharm/src/main/java/net/ashald/envfile/products/pycharm/PythonProvider.java
@@ -3,6 +3,7 @@ package net.ashald.envfile.products.pycharm;
 import com.intellij.execution.ExecutionException;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.jetbrains.python.console.PydevConsoleRunnerImpl;
 import com.jetbrains.python.run.AbstractPythonRunConfiguration;
 import com.jetbrains.python.run.PythonExecution;
 import com.jetbrains.python.run.PythonRunParams;
@@ -17,11 +18,12 @@ public class PythonProvider implements PythonCommandLineTargetEnvironmentProvide
     private final Logger LOG = Logger.getInstance("envfile");
     @Override
     public void extendTargetEnvironment(@NotNull Project project, @NotNull HelpersAwareTargetEnvironmentRequest helpersAwareTargetEnvironmentRequest, @NotNull PythonExecution pythonExecution, @NotNull PythonRunParams pythonRunParams) {
+        if (pythonRunParams instanceof PydevConsoleRunnerImpl.PythonConsoleRunParams) return;
+        final AbstractPythonRunConfiguration<?> params = (AbstractPythonRunConfiguration<?>) pythonRunParams;
+
         try {
-            EnvFileConfigurationEditor.collectEnv(
-                (AbstractPythonRunConfiguration<?>) pythonRunParams,
-                new HashMap<>(pythonRunParams.getEnvs())
-            ).forEach(pythonExecution::addEnvironmentVariable);
+            EnvFileConfigurationEditor.collectEnv(params, new HashMap<>(pythonRunParams.getEnvs()))
+                    .forEach(pythonExecution::addEnvironmentVariable);
         } catch (ExecutionException e) {
             LOG.error(e);
         }
